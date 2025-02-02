60c36e72bc2c9f91246a11adf8283c5ce0c3af4d
the boy <info.idoko@gmail.com>
2025-02-02 02:30:18 +0100

    Add files via upload

--- pkg/controllers/NotificationMail.go
+++ pkg/controllers/NotificationMail.go
@@ -1,49 +1,66 @@
-package controller
+package main
 
 import (
 	"fmt"
 	"net/http"
 	"os"
-	"puppyspot-backend/pkg/utils"
 	"time"
 
-	"github.com/go-mail/mail"
+	mail "github.com/go-mail/mail/v2"
 )
 
-
-
-
 func HandleNotification(w http.ResponseWriter, r *http.Request) {
-	utils.EnableCors(w, r)
-	// Parse multipart form
-	err := r.ParseMultipartForm(10 << 20) // 10 MB maximum file size
+	utils.EnableCors(w, r) // Ensure CORS is enabled properly
+
+	// Log request headers to debug the content type
+	fmt.Println("Received Headers:", r.Header)
+
+	// Parse multipart form data
+	err := r.ParseMultipartForm(10 << 20) // 10 MB max size
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, "Failed to parse multipart form", http.StatusBadRequest)
+		fmt.Println("Error parsing multipart form:", err)
 		return
-	} else {
-		// Get form values
-		documentID := r.FormValue("documentID")
-		message := r.FormValue("message")
-
-
-		var emailAdd = os.Getenv("EMAIL")
-		var emailPassword = os.Getenv("APP_PASSWORD")
-		var emailHost = os.Getenv("EMAIL_HOST")
-
-		// Create a new mailer
-		m := mail.NewMessage()
-		m.SetHeader("From", emailAdd)
-		m.SetHeader("To", "info.idoko@gmail.com")
-		m.SetAddressHeader("Cc", emailAdd, "Puppy Spot")
-		m.SetHeader("Subject", "Notification!!!")
-
-		m.SetBody("text/html", "Someone just triggered something: <br/> <p>ID: "+documentID+"</p> <br/> <p>Message: "+message+"</p>")
-	
-		
-		// Send email
-		d := mail.NewDialer(emailHost, 465, emailAdd, emailPassword)
-		d.Timeout = 120 * time.Second
-		d.StartTLSPolicy = mail.MandatoryStartTLS		
-
 	}
+
+	// Get form values
+	documentID := r.FormValue("documentID")
+	message := r.FormValue("message")
+
+	fmt.Println("Received documentID:", documentID)
+	fmt.Println("Received message:", message)
+
+	// Ensure environment variables are set
+	emailAdd := os.Getenv("EMAIL")
+	emailPassword := os.Getenv("APP_PASSWORD")
+	emailHost := os.Getenv("EMAIL_HOST")
+
+	if emailAdd == "" || emailPassword == "" || emailHost == "" {
+		http.Error(w, "Email configuration is missing", http.StatusInternalServerError)
+		return
+	}
+
+	// Create a new mailer
+	m := mail.NewMessage()
+	m.SetHeader("From", emailAdd)
+	m.SetHeader("To", "info.idoko@gmail.com")
+	m.SetAddressHeader("Cc", emailAdd, "Puppy Spot")
+	m.SetHeader("Subject", "Notification!!!")
+	m.SetBody("text/html", fmt.Sprintf("Someone just triggered something:<br/><p>ID: %s</p><br/><p>Message: %s</p>", documentID, message))
+
+	// Send email
+	d := mail.NewDialer(emailHost, 465, emailAdd, emailPassword)
+	d.Timeout = 120 * time.Second
+	d.StartTLSPolicy = mail.MandatoryStartTLS
+
+	// Attempt to send email
+	if err := d.DialAndSend(m); err != nil {
+		http.Error(w, "Failed to send email", http.StatusInternalServerError)
+		fmt.Println("Error sending email:", err)
+		return
+	}
+
+	// Send a success response to the client
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Notification sent successfully"))
 }
