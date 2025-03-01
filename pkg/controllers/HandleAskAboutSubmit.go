6e153adf4e3a11337048cede0ce85cca9da2eb72
the boy <info.idoko@gmail.com>
2025-03-01 08:16:50 +0100

    Update HandleAskAboutSubmit.go

--- pkg/controllers/HandleAskAboutSubmit.go
+++ pkg/controllers/HandleAskAboutSubmit.go
@@ -29,33 +29,37 @@ func HandleAskAboutMail (w http.ResponseWriter, r *http.Request){
 	var emailPassword = os.Getenv("APP_PASSWORD")
 	var emailHost = os.Getenv("EMAIL_HOST")
 
-	// Create a new mailer
-	m := mail.NewMessage()
-	m.SetHeader("From", emailAdd)
-	m.SetHeader("To", "info.idoko@gmail.com")
-	m.SetAddressHeader("Cc", emailAdd, "Puppy Spot")
-	m.SetHeader("Subject", "WANT TO KNOW MORE")
-	m.SetBody("text/html", "<h1>Hello White,</h1><br><p>someone wants to know more about this puppy: "+puppyID+", <strong>Congratulations!!!</strong></p><br><p>details of asker, are as followed</p><br><ul><li>First Name : "+firstName+" </li><li>Last Name: "+lastName+" </li><li>Email: "+emailAddress+" </li><li>phone: "+phone+" </li><li>state: "+state+" </li><li>text: "+text+" </li></ul>")
+	if firstName != nil{
+		// Create a new mailer
+		m := mail.NewMessage()
+		m.SetHeader("From", emailAdd)
+		m.SetHeader("To", "info.idoko@gmail.com")
+		m.SetAddressHeader("Cc", emailAdd, "Puppy Spot")
+		m.SetHeader("Subject", "WANT TO KNOW MORE")
+		m.SetBody("text/html", "<h1>Hello White,</h1><br><p>someone wants to know more about this puppy: "+puppyID+", <strong>Congratulations!!!</strong></p><br><p>details of asker, are as followed</p><br><ul><li>First Name : "+firstName+" </li><li>Last Name: "+lastName+" </li><li>Email: "+emailAddress+" </li><li>phone: "+phone+" </li><li>state: "+state+" </li><li>text: "+text+" </li></ul>")
 
-	// Send email
-	d := mail.NewDialer(emailHost, 465, emailAdd, emailPassword)
-	d.Timeout = 120 * time.Second
-	d.StartTLSPolicy = mail.MandatoryStartTLS
+		// Send email
+		d := mail.NewDialer(emailHost, 465, emailAdd, emailPassword)
+		d.Timeout = 120 * time.Second
+		d.StartTLSPolicy = mail.MandatoryStartTLS
 
-	if err := d.DialAndSend(m); err != nil {
-		var newfailureMessage FailureMessage
-		newfailureMessage.Success = false
-		newfailureMessage.ErrorNumber = 2
-		newfailureMessage.Message = "fail to send mall"
+		if err := d.DialAndSend(m); err != nil {
+			var newfailureMessage FailureMessage
+			newfailureMessage.Success = false
+			newfailureMessage.ErrorNumber = 2
+			newfailureMessage.Message = "fail to send mall"
 
-		json.NewEncoder(w).Encode(newfailureMessage)
-		fmt.Println(err)
-		// panic(err)
-	} else {
-		var newSuccessMessage SuccessMessage
-		newSuccessMessage.Success = true
-		json.NewEncoder(w).Encode(newSuccessMessage)
+			json.NewEncoder(w).Encode(newfailureMessage)
+			fmt.Println(err)
+			// panic(err)
+		} else {
+			var newSuccessMessage SuccessMessage
+			newSuccessMessage.Success = true
+			json.NewEncoder(w).Encode(newSuccessMessage)
 
+		}
 	}
+
+	
 
 }
